e39ae1c2adc0ae3c8e6d7287946f1d5db06c0c46
Baklaikin Kostiantyn <baklaikin@gmail.com>
2021-06-04 21:30:51 +0300

    Update task-5

--- js/task-05.js
+++ js/task-05.js
@@ -4,6 +4,10 @@ const outputText = document.querySelector('#name-output');
 inputField.addEventListener('input', onFormInput);
 
 function onFormInput(event) {
+    if (event.currentTarget.value === '') {
+        outputText.textContent = 'незнакомец';
+        return;
+    }
     outputText.textContent = event.currentTarget.value;
 };
 
